99c9384baede8e22bd1e7848cbc8ab28ba6b8ca1
Dylan Nissley <dylan.nissley@gmail.com>
2020-06-07 21:06:41 -0500

    feat: add express-routes-file example

---
+++ express-routes-file-example/src/index.ts
@@ -0,0 +1,13 @@
+import * as express from 'express'
+import configureRoutes from 'express-routes-file'
+
+const app = express()
+
+const routes = configureRoutes({
+  hello: (req, res) => res.send('hello world'),
+  hi: (req, res) => res.send('hi world')
+})
+
+app.use('/', routes)
+
+app.listen(3000, () => console.log('listening on port 3000'))
